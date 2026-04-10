1bcafc74d81a62427fed81541d169d2056729da2
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-04-10 07:22:38 +0100

    format

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 console.log("hello world");
 const films = [
-    {
-  title: "Killing of Flower Moon",
-  director: "Martin Scorsese",
-  times: ["15:35"],
-  certificate: "15",
-  duration: 112,
-},
-{
-  title: "Typist Artist Pirate King",
-  director: "Carol Morley",
+  {
+    title: "Killing of Flower Moon",
+    director: "Martin Scorsese",
+    times: ["15:35"],
+    certificate: "15",
+    duration: 112,
+  },
+  {
+    title: "Typist Artist Pirate King",
+    director: "Carol Morley",
     times: ["12:00", "18:00"],
     certificate: "12A",
     duration: 108,
-    },
+  },
 ];
 
 //Goal: How to use film card component with an array
@@ -21,22 +21,20 @@ const films = [
 //Goal: How can we reuse functionality?
 
 function createFilmCard(film) {
-    // How we go through the array of films and create a card for each element
-const filmCard = document
+  // How we go through the array of films and create a card for each element
+  const filmCard = document
     .getElementById("film-card-template")
     .content.cloneNode(true);
 
-    filmCard.querySelector("h3").textContent = film.title;
-    filmCard.querySelector("p").textContent = film.director;
-    return filmCard;
+  filmCard.querySelector("h3").textContent = film.title;
+  filmCard.querySelector("p").textContent = film.director;
+  return filmCard;
 }
 
 // How we go through the array of films and create a card for each element
-    
+
 for (const film of films) {
-   const card = createFilmCard(film);
-   console.log(card);
-    document.body.appendChild(card);
+  const card = createFilmCard(film);
+  console.log(card);
+  document.body.appendChild(card);
 }
-
-
