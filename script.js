562e7224ae5ae8447660a57fb601734b8154ce30
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-04-10 07:20:55 +0100

    iterate through films to render each object as film card

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 console.log("hello world");
-const film1 = {
+const films = [
+    {
   title: "Killing of Flower Moon",
   director: "Martin Scorsese",
   times: ["15:35"],
   certificate: "15",
   duration: 112,
-};
-const film2 = {
+},
+{
   title: "Typist Artist Pirate King",
   director: "Carol Morley",
     times: ["12:00", "18:00"],
     certificate: "12A",
     duration: 108,
-    };
-
+    },
+];
 
 //Goal: How to use film card component with an array
 
 //Goal: How can we reuse functionality?
 
 function createFilmCard(film) {
-  const filmCard = document
+    // How we go through the array of films and create a card for each element
+const filmCard = document
     .getElementById("film-card-template")
     .content.cloneNode(true);
 
-  filmCard.querySelector("h3").textContent = film.title;
-  filmCard.querySelector("p").textContent = film.director;
-  return filmCard;
+    filmCard.querySelector("h3").textContent = film.title;
+    filmCard.querySelector("p").textContent = film.director;
+    return filmCard;
 }
-document.body.append(createFilmCard(film1), createFilmCard(film2));
+
+// How we go through the array of films and create a card for each element
+    
+for (const film of films) {
+   const card = createFilmCard(film);
+   console.log(card);
+    document.body.appendChild(card);
+}
+
+
