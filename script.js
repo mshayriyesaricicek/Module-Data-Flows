42fa292af10fe8d0cbcc90f851981a192496cd17
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-04-10 06:36:15 +0100

    make a film card function

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 console.log("hello world");
-const film = {
+const film1 = {
   title: "Killing of Flower Moon",
   director: "Martin Scorsese",
   times: ["15:35"],
   certificate: "15",
   duration: 112,
 };
-console.log(film.title, "<---- film.title");
-
-//Goal: Refactor this logic to use a film card template instead`
-
-const filmCard = document
-  .getElementById("film-card-template")
-  .content.cloneNode(true);
-
-console.log(filmCard);
-
-filmCard.querySelector("h3").textContent = film.title;
-filmCard.querySelector("p").textContent = film.director;
-document.body.appendChild(filmCard);
+const film2 = {
+  title: "Typist Artist Pirate King",
+  director: "Carol Morley",
+    times: ["12:00", "18:00"],
+    certificate: "12A",
+    duration: 108,
+    };
 
 
+//Goal: How to use film card component with an array
+
+//Goal: How can we reuse functionality?
+
+function createFilmCard(film) {
+  const filmCard = document
+    .getElementById("film-card-template")
+    .content.cloneNode(true);
+
+  filmCard.querySelector("h3").textContent = film.title;
+  filmCard.querySelector("p").textContent = film.director;
+  return filmCard;
+}
+document.body.append(createFilmCard(film1), createFilmCard(film2));
